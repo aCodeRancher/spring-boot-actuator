02ab4de87fcc7b4f9686bfc699ef1139826c9ea0
helen <hhma@alumni.cpp.edu>
2021-01-06 21:07:10 -0500

    remove Map and HashMap.

--- src/main/java/guru/springframework/actuator/endpoints/CustomGuruEndpoint.java
+++ src/main/java/guru/springframework/actuator/endpoints/CustomGuruEndpoint.java
@@ -3,10 +3,7 @@ package guru.springframework.actuator.endpoints;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 @Component
