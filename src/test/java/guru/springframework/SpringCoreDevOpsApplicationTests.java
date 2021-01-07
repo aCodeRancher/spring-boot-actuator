5c96bd16f0562585c49a12be1bcb1bda90749a0f
helen <hhma@alumni.cpp.edu>
2021-01-07 18:19:56 -0500

    use JUnit 5.

--- src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
+++ src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
@@ -1,15 +1,12 @@
 package guru.springframework;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SpringCoreDevOpsApplication.class)
-@WebAppConfiguration
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+
 public class SpringCoreDevOpsApplicationTests {
 
 	@Test
