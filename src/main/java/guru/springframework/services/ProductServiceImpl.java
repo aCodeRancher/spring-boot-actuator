4f479f122e0ad956b5f5505c094ae542c0616c67
helen <hhma@alumni.cpp.edu>
2021-01-08 22:57:46 -0500

    remove the old metrics.

--- src/main/java/guru/springframework/services/ProductServiceImpl.java
+++ src/main/java/guru/springframework/services/ProductServiceImpl.java
@@ -5,10 +5,7 @@ import guru.springframework.repositories.ProductRepository;
 import guru.springframework.services.jms.JmsTextMessageService;
 import org.apache.commons.collections4.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.CounterService;
-import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 /**
@@ -19,33 +16,25 @@ public class ProductServiceImpl implements ProductService {
 
     private ProductRepository productRepository;
     private JmsTextMessageService jmsTextMessageService;
-    private CounterService counterService;
-    private GaugeService gaugeService;
 
     @Autowired
-    public ProductServiceImpl(ProductRepository productRepository, JmsTextMessageService jmsTextMessageService,
-                              CounterService counterService, GaugeService gaugeService) {
+    public ProductServiceImpl(ProductRepository productRepository, JmsTextMessageService jmsTextMessageService) {
         this.productRepository = productRepository;
         this.jmsTextMessageService = jmsTextMessageService;
-        this.counterService = counterService;
-        this.gaugeService = gaugeService;
+
     }
 
     @Override
     public Product getProduct(Integer id) {
         jmsTextMessageService.sendTextMessage("Fetching Product ID: " + id );
-        counterService.increment("guru.springframework.services.ProductService.getproduct");
-        return productRepository.findOne(id);
+
+        return productRepository.findById(id).orElseThrow();
     }
 
     @Override
     public List<Product> listProducts() {
         jmsTextMessageService.sendTextMessage("Listing Products");
-
-        counterService.increment("guru.springframework.services.ProductService.listProducts");
-        gaugeService.submit("guru.springframework.services.listProducts.ProductService.pageViewsPerMin", 6);
-
-        return IteratorUtils.toList(productRepository.findAll().iterator());
+       return IteratorUtils.toList(productRepository.findAll().iterator());
     }
 
 }
