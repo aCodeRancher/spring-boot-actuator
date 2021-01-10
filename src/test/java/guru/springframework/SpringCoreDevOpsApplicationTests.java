1a58714ca4815d4a510c3b70373d2f2b94d379d5
helen <hhma@alumni.cpp.edu>
2021-01-10 11:07:59 -0500

    add a test for Chuck Norris.

--- src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
+++ src/test/java/guru/springframework/SpringCoreDevOpsApplicationTests.java
@@ -1,13 +1,34 @@
 package guru.springframework;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-@SpringBootTest(classes = SpringCoreDevOpsApplication.class)
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SpringCoreDevOpsApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+    @Autowired
+	ObjectMapper objectMapper;
+
 	@Test
-	public void contextLoads() {
+	public void chuckNorrisTest() throws Exception {
+		MvcResult mvcResult =  mockMvc.perform(get("/actuator/info"))
+				 .andReturn();
+		String infoOutput = mvcResult.getResponse().getContentAsString();
+		JsonNode jsonNode = objectMapper.readTree(infoOutput);
+		String chuckNorris = jsonNode.get("Chuck Norris").asText();
+		assertTrue(chuckNorris.length()>0);
 	}
 
 }
